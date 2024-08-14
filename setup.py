2d7940523eb07e5dc86b751ef75323c238c9b343
Evan Amedi <140571286+evanamedi@users.noreply.github.com>
2024-08-14 14:53:16 -0400

    Fixed syntax error in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="langman",
-	version="1.0.0",
-	packages=find_packages(where='src'),
-	package_dir={'': 'src'},
-	entry_points={
-		'console_scripts': [
-			'langman=langman:main',
-		],
-	},
-	install_requires=[],
-	include_package_data=True,
-	zip_safe=False,
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-	python_requires'>=3.12',
+    name="langman",  # This is the name of the package
+    version="1.0.0",  # Package version
+    packages=find_packages(where='src'),  # This specifies where to find the packages
+    package_dir={'': 'src'},  # This tells setuptools that packages are under 'src'
+    entry_points={
+        'console_scripts': [
+            'langman=langman:main',  # The command `langman` will execute `main` in `langman.py`
+        ],
+    },
+    install_requires=[],  # List your dependencies here or in requirements.txt
+    include_package_data=True,  # This includes non-Python files specified in MANIFEST.in
+    zip_safe=False,  # This prevents setuptools from zipping the package, which can cause issues
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify the minimum Python version
 )
